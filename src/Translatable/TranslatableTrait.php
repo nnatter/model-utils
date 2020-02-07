539e4ac9d36d9d3e9df218d8b597603e2b0bf80d
Luca Rath-Heel <luca@sulu.io>
2020-02-07 16:06:04 +0100

    add test setup

--- src/Translatable/TranslatableTrait.php
+++ src/Translatable/TranslatableTrait.php
@@ -6,8 +6,8 @@ namespace HandcraftedInTheAlps\Util\Model\Translatable;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Sulu\Bundle\DirectoryBundle\Common\Translatable\Exception\LocaleNotSetException;
-use Sulu\Bundle\DirectoryBundle\Common\Translatable\Exception\TranslationNotFoundException;
+use HandcraftedInTheAlps\Util\Model\Translatable\Exception\LocaleNotSetException;
+use HandcraftedInTheAlps\Util\Model\Translatable\Exception\TranslationNotFoundException;
 
 trait TranslatableTrait
 {
